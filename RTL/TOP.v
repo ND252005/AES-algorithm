f9d7fd8f9427bc9a09576edfcc18940871069625
ND250205 <nguyenduc250205@gmail.com>
2025-11-22 15:27:33 +0700

    update full aes 128

--- RTL/TOP.v
+++ RTL/TOP.v
@@ -4,30 +4,29 @@ module TOP #(
     parameter DATA_LEN = 128,
     parameter NUMS_OF_ROUND = 10
 ) (
-    input clk,
+    input clk, //Clock và reset của hệ thống
     input reset,
-    input data_valid_in,
-    input [DATA_LEN-1 : 0] plain_text,
-    input key_valid_in,
-    input [KEY_LEN-1 : 0] cipher_key,
-    output reg data_valid_out,
-    output reg [DATA_LEN-1 : 0] cipher_text
+    input data_valid_in, // biến kiểm tra dữ liệu đầu vào có hay chưa
+    input [DATA_LEN-1 : 0] plain_text, // dữ liệu cần mã hóa
+    input key_valid_in, //biến kiểm ra khóa đầu vào có hay chưa
+    input [KEY_LEN-1 : 0] cipher_key, // khóa đầu vào
+    output wire data_valid_out, // biến kiểm tra dữ liệu đầu ra có hay chưa
+    output wire [DATA_LEN-1 : 0] cipher_text // dữ liệu sau khi mã hóa
 );
-    wire [NUMS_OF_ROUND : 0] subkey_valid_out;
-    wire [(NUMS_OF_ROUND*KEY_LEN-1) : 0] key_array;
-    wire [NUMS_OF_ROUND-1 : 0] round_valid_in;
-    wire [DATA_LEN-1 : 0] round_data_in [0 : NUMS_OF_ROUND-2];
+    wire [NUMS_OF_ROUND-1 : 0] subkey_valid; // biến kiểm tra khóa con của từng vòng có hay chưa
+    wire [(NUMS_OF_ROUND*KEY_LEN-1) : 0] key_array; //  mảng khóa con của tất cả các vòng
+    wire [NUMS_OF_ROUND-1 : 0] round_valid; // biến kiểm tra dữ liệu đầu ra của từng vòng có hay chưa
+    wire [DATA_LEN-1 : 0] round_data [0 : NUMS_OF_ROUND-1]; // dữ liệu đầu ra của từng vòng
 
 // khai báo dây cho vòng cuối cùng
-    wire [DATA_LEN-1 : 0] shift_data_in; // dữ liệu đầu ra của SubByte() là đầu vào của ShiftRows()
-    wire [DATA_LEN-1 : 0] addrk_data_in; // dữ liệu đầu ra của MixColums() là đầu vào của AddRoundKey()
-    wire [DATA_LEN-1 : 0] final_data;
+    wire [DATA_LEN-1 : 0] data_sub2shift; // dữ liệu đầu ra của SubByte() là đầu vào của ShiftRows()
 
-    wire shift_valid_in; // tín hiệu đầu ra của SubByte() là đầu vào của ShiftRows()
+    wire valid_sub2shift; // tín hiệu đầu ra của SubByte() là đầu vào của ShiftRows()
     wire addrk_valid_in; // tín hiệu đầu ra của MixColums() là đầu vào của AddRoundKey()
-    wire final_valid;
 
 //delay cho vòng cuối để pipeline cho mixcolunms
+    wire[DATA_LEN-1:0] data_shift2key;           //for delay register
+    wire valid_shift2key;
     reg[DATA_LEN-1:0] data_shift2key_delayed;           //for delay register
     reg valid_shift2key_delayed;
 
@@ -40,7 +39,7 @@ module TOP #(
         .valid_in(key_valid_in),
         .Secret_key(cipher_key),
         .key_expan(key_array),
-        .valid_out(subkey_valid_out)
+        .valid_out(subkey_valid)
     );
 
     AddRoundKey #(
@@ -52,8 +51,8 @@ module TOP #(
         .data_in(plain_text),
         .key_valid_in(key_valid_in),
         .round_key(cipher_key),
-        .valid_out(round_valid_in[0]),
-        .data_out(round_data_in[0])
+        .valid_out(round_valid[0]),
+        .data_out(round_data[0])
     );
 
     genvar i;
@@ -64,27 +63,26 @@ generate
         ) ROUND_inst (
             .clk(clk),
             .reset(reset),
-            .data_valid_in(round_valid_in[i-1]),
-            .data_in(round_data_in[i-1]),
-            .key_valid_in(subkey_valid_out[i-1]),
-            .sub_key(key_array[i-1]),
-            .valid_out(round_valid_in[i]),
-            .data_valid_out(key_array[i])
+            .data_valid_in(round_valid[i-1]),
+            .data_in(round_data[i-1]),
+            .key_valid_in(subkey_valid[i-1]),
+            .sub_key(key_array[(i*KEY_LEN-1) : ((i-1)*KEY_LEN)]),
+            .valid_out(round_valid[i]),
+            .data_out(round_data[i])
         );
     end 
 endgenerate
 
 // -----Last round: SubBytes ()-----
-
     SubBytes #(
         .DATA_LEN(DATA_LEN)
     ) SubBytes_last (
             .clk(clk), 
             .reset(reset),
-            .valid_in(round_valid_in[NUMS_OF_ROUND-1]),
-            .data_in(round_data_in[NUMS_OF_ROUND-1]),
-            .valid_out(shift_valid_in),
-            .data_out(shift_data_in)
+            .valid_in(round_valid[NUMS_OF_ROUND-1]),
+            .data_in(round_data[NUMS_OF_ROUND-1]),
+            .valid_out(valid_sub2shift),
+            .data_out(data_sub2shift)
     );
 
 //-----Last round: ShiftRows()-----
@@ -93,8 +91,8 @@ endgenerate
     ) ShiftRows_last (
         .clk(clk),
         .reset(reset),
-        .valid_in(shift_valid_in),
-        .data_in(shift_data_in),
+        .valid_in(valid_sub2shift),
+        .data_in(data_sub2shift),
         .valid_out(valid_shift2key),
         .data_out(data_shift2key)
     );
@@ -107,9 +105,9 @@ endgenerate
         .reset(reset),
         .data_valid_in(valid_shift2key_delayed),
         .data_in(data_shift2key_delayed),
-        .key_valid_in(key_valid_in),
-        .round_key(sub_key),
-        .valid_out(valid_out),
+        .key_valid_in(subkey_valid[NUMS_OF_ROUND-1]),
+        .round_key(key_array[(NUMS_OF_ROUND*KEY_LEN-1) : ((NUMS_OF_ROUND-1)*KEY_LEN)]),
+        .valid_out(data_valid_out),
         .data_out(cipher_text)
     );
 
@@ -119,22 +117,12 @@ endgenerate
         valid_shift2key_delayed <= 1'b0;
         data_shift2key_delayed <= 'b0;
     end else begin
-
-    if(valid_shift2key)begin
-    data_shift2key_delayed <= data_shift2key;
+        if(valid_shift2key)begin
+            data_shift2key_delayed <= data_shift2key;
+        end else begin
+            data_shift2key_delayed = 'b0;
+        end
+        valid_shift2key_delayed <= valid_shift2key;
     end
-    valid_shift2key_delayed <= valid_shift2key;
-    end
-
-    // --- Output Assignment ---
-    // always @(posedge clk or posedge reset) begin
-    //     if (reset) begin
-    //         data_valid_out <= 0;
-    //         cipher_text    <= 0;
-    //     end else begin
-    //         data_valid_out <= final_valid;
-    //         cipher_text    <= final_data;
-    //     end
-    // end
 
 endmodule
--- testbench/TOP_tb.v
+++ testbench/TOP_tb.v
@@ -1,11 +1,73 @@
 `timescale 1ns/1ps
 module TOP_tb ();
-    reg clk, reset;
+    parameter KEY_LEN = 128;
+    parameter DATA_LEN = 128;
+    parameter NUMS_OF_ROUND = 10;
+
+    reg clk, reset; 
     reg data_valid_in;
     reg [DATA_LEN-1 : 0] plain_text;
     reg key_valid_in;
     reg [KEY_LEN-1 : 0] cipher_key;
     wire data_valid_out;
     wire [DATA_LEN-1 : 0] cipher_text;
+
+    TOP #(
+        .KEY_LEN(KEY_LEN),
+        .DATA_LEN(DATA_LEN),
+        .NUMS_OF_ROUND(NUMS_OF_ROUND)
+    ) AES_algorithm (
+        .clk(clk),
+        .reset(reset),
+        .data_valid_in(data_valid_in),
+        .plain_text(plain_text),
+        .key_valid_in(key_valid_in),
+        .cipher_key(cipher_key),
+        .data_valid_out(data_valid_out),
+        .cipher_text(cipher_text)
+    );
+
+    // tạo xung clk
+    initial begin
+        clk = 0;
+        forever begin
+            #5 clk = ~clk;
+        end
+    end
+    // monitor output
+    always @(posedge clk) begin
+        if (data_valid_out) begin
+            $display("Time = %0t ns", $time);
+            $display("Cipher text output = %h", cipher_text);
+        end
+    end
+
+    initial begin
+        // init signals
+        reset = 0;
+        data_valid_in = 0;
+        key_valid_in  = 0;
+        plain_text = 0;
+        cipher_key = 0;
+        key_valid_in = 0;
+
+        // ---- Test vector (có thể thay đổi) ----
+        cipher_key = 128'h000102030405060708090A0B0C0D0E0F;
+        plain_text = 128'h00112233445566778899AABBCCDDEEFF;
+        
+        #10; 
+        key_valid_in = 0;
+        data_valid_in = 0;
+
+        #20;
+        reset = 1;
+        data_valid_in = 1;
+        key_valid_in = 1;
+        $display("Time = %0t ns", $time);
+        $display("plain text input = %h | cipher key input = %h", plain_text, cipher_key);
+        // thêm delay cho chắc
+        #1000;
+        $finish;
+    end
 
 endmodule
